87b0196785311a088b5e00dcfa5633091a1f334c
DoidoYo <gabrielblfernandes@gmail.com>
2024-12-28 22:24:22 -0500

    migration-update

--- server/migration/1735431102803-AddFollowIdsToMedia.ts
+++ server/migration/1735431102803-AddFollowIdsToMedia.ts
@@ -6,12 +6,24 @@ export class AddFollowIdsToMedia1735431102803 implements MigrationInterface {
     await queryRunner.query(
         `ALTER TABLE "media" ADD COLUMN "followIds" text DEFAULT '[]'`
     );
+    await queryRunner.query(
+      `ALTER TABLE "season" ADD COLUMN "totalEpisodesNumber" integer DEFAULT 0`
+    );
+  await queryRunner.query(
+      `ALTER TABLE "season" ADD COLUMN "episodesNumber" integer DEFAULT 0`
+  );
 }
 
 public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-        `ALTER TABLE "media" DROP COLUMN "followIds"`
-    );
+  await queryRunner.query(
+      `ALTER TABLE "media" DROP COLUMN "followIds"`
+  );
+  await queryRunner.query(
+    `ALTER TABLE "season" DROP COLUMN "totalEpisodesNumber"`
+  );
+  await queryRunner.query(
+      `ALTER TABLE "season" DROP COLUMN "episodesNumber"`
+  );
 }
 
 }
