c2915d42f88a9813d39535e13976c5e972addd1b
DoidoYo <gabrielblfernandes@gmail.com>
2025-01-13 22:32:38 -0500

    Update 1735431102803-AddFollowIdsToMedia.ts

--- server/migration/1735431102803-AddFollowIdsToMedia.ts
+++ server/migration/1735431102803-AddFollowIdsToMedia.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import type { MigrationInterface, QueryRunner } from "typeorm"
 
 export class AddFollowIdsToMedia1735431102803 implements MigrationInterface {
 
